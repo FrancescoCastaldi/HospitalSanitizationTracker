b6fde3204ad00540c63695fc31f9607b474f4890
FrancescoCastaldi <francesco.castaldi4@studio.unibo.it>
2026-03-12 00:56:58 +0100

    fix bug

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,14 @@
 const hre = require("hardhat");
 
-async function main() {
+async function main() { // Funzione principale che esegue il processo di deploy del contratto
   console.log("Deploying SanitizationTracker...");
 
-  const SanitizationTracker = await hre.ethers.getContractFactory("SanitizationTracker");
+  const SanitizationTracker = await hre.ethers.getContractFactory("SanitizationTracker"); // Ottiene il factory del contratto SanitizationTracker, che permette di creare nuove istanze del contratto
   const contract = await SanitizationTracker.deploy();
 
   await contract.waitForDeployment();
 
-  const address = await contract.getAddress();
+  const address = await contract.getAddress(); // Ottiene l'indirizzo del contratto appena distribuito sulla blockchain
   console.log("SanitizationTracker deployed to:", address);
 }
 
